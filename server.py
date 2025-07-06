70d57246cf2f107056c0c65bc265ecc5f70d2e07
cyu-xian <thg7091@gmail.com>
2025-07-07 01:59:11 +0800

    Testting launch

--- server.py
+++ server.py
@@ -452,6 +452,9 @@ def authenticate():
         }), 500
 # authentication api end
 
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host="0.0.0.0", port=port)
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
